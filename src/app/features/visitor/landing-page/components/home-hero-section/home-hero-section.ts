c5d96047ee1f020ae39026835c015c1c78fc9658
EiriniAl <eiraloupi01@gmail.com>
2026-05-18 20:49:06 +0300

    connected landing page to nominatim service, tried to fix accpage issues

--- src/app/features/visitor/landing-page/components/home-hero-section/home-hero-section.ts
+++ src/app/features/visitor/landing-page/components/home-hero-section/home-hero-section.ts
@@ -4,11 +4,12 @@ import { InputIconModule } from 'primeng/inputicon';
 import { InputTextModule } from 'primeng/inputtext';
 import { FloatLabel } from 'primeng/floatlabel';
 import { Specialty } from '../../../../../shared/domain/specialty';
+import { DoctorSearchService } from '../../../../../shared/services/doctor-search-service';
 
 
 @Component({
   selector: 'app-home-hero-section',
-  imports: [IconFieldModule, InputIconModule, InputTextModule, FloatLabel],
+  imports: [IconFieldModule, InputIconModule, InputTextModule],
   templateUrl: './home-hero-section.html',
   styleUrl: './home-hero-section.scss',
 })
@@ -17,12 +18,29 @@ export class HomeHeroSection {
 
   @Input() locations: any[] = [];
 
+  @Input() specialtyQuery = '';
+  @Input() locationQuery = '';
+
+  @Output() specialtyQueryChange = new EventEmitter<string>();
+  @Output() locationQueryChange = new EventEmitter<string>();
   @Output() locationSearch = new EventEmitter<string>();
+  @Output() searchClicked = new EventEmitter<void>();
+
+  constructor(private doctorSearchService:DoctorSearchService){
+
+  }
+
+  onSpecialtyInput(event: Event) {
+    const query = (event.target as HTMLInputElement).value;
+    this.specialtyQueryChange.emit(query);
+  }
 
   onLocationInput(event: Event) {
     const query = (event.target as HTMLInputElement).value;
+    this.locationQueryChange.emit(query);
     if (query.length > 2) {
       this.locationSearch.emit(query);
     }
   }
+
 }
